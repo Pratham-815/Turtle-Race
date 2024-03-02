11bb56434bee760c71a49f9d4702c73e26b622dd
Pratham Agarwal <prathamagarwal815@gmail.com>
2024-03-02 12:31:18 +0530

    Import random module and create turtle list to add all turtle

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from turtle import Turtle, Screen
+import random
 
+is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color")
 
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
+all_turtles = []
+
 for i in range(6):
     y_coordinate = -100 + i * 40
-    timmy = Turtle(shape="turtle")
-    timmy.penup()
-    timmy.goto(x=-230, y=y_coordinate)
-    timmy.color(colors[i])
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_coordinate)
+    new_turtle.color(colors[i])
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    pass
 
 screen.exitonclick()
