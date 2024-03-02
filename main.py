080ac3753400ba56a6aba82dba50d0cdb1971c3f
Pratham Agarwal <prathamagarwal815@gmail.com>
2024-03-02 12:21:46 +0530

    Initialize turtle 6 time with coordinate, color and shape

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from turtle import Turtle, Screen
 
-timmy = Turtle()
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color")
+
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+for i in range(6):
+    y_coordinate = -100 + i * 40
+    timmy = Turtle(shape="turtle")
+    timmy.penup()
+    timmy.goto(x=-230, y=y_coordinate)
+    timmy.color(colors[i])
+
+screen.exitonclick()
